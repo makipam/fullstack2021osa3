39218bef9290d3db554f6465ba272bc94647aff2
Max Mäkipää <max.makipaa@gmail.com>
2021-10-11 14:14:01 +0300

    uibuild

--- index.js
+++ index.js
@@ -41,22 +41,20 @@ app.get('/info', (req, res) => {
   })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-    
-  if (body.name === undefined || body.number === undefined) {
-    return response.status(400).json({ 
-      error: 'name or number missing' })
-  }
-    
+
   const person = new Person({
     name: body.name,
     number: body.number
   })
     
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
+  person.save()
+  .then(savedPerson => savedPerson.toJSON())
+  .then(savedAndFormattedPerson => {
+    response.json(savedAndFormattedPerson)
   })
+  .catch(error => next(error))
 })
 
 
@@ -113,6 +111,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message})
   }
 
   next(error)
