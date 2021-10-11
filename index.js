47237b074dfbf079b35a3897ac5787682dc9dea6
Max Mäkipää <max.makipaa@gmail.com>
2021-10-11 11:50:29 +0300

    uibuild

--- index.js
+++ index.js
@@ -18,16 +18,6 @@ app.use(morgan(' :method :url :status :res[content-length] - :response-time ms :
 app.use(cors())
 app.use(express.static('build'))
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-app.use(requestLogger)
-
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
@@ -99,6 +89,16 @@ app.delete('/api/persons/:id', (request, response, next) => {
   })
   .catch(error => next(error))
 })
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
