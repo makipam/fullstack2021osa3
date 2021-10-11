431ccff341a8ed786118de7f2f32d4ec94e73940
Max Mäkipää <max.makipaa@gmail.com>
2021-10-11 11:09:23 +0300

    uibuild

--- index.js
+++ index.js
@@ -11,47 +11,45 @@ morgan.token("note", (req, res) => {
   } else {
   return JSON.stringify(req.body)
   }
-});
+})
 
 app.use(express.json())
 app.use(morgan(' :method :url :status :res[content-length] - :response-time ms :note'))
 app.use(cors())
 app.use(express.static('build'))
+app.use(requestLogger)
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
   })
 
-  app.get('/info', (req, res) => {
-    const amount = persons.length
-    const date = new Date()
-    res.send
-    (`<p>Phonebook has info for ${persons.length} people </p> <p> ${date} </p>`)
-  })
+app.get('/info', (req, res) => {
+  const amount = persons.length
+  const date = new Date()
+  res.send
+  (`<p>Phonebook has info for ${persons.length} people </p> <p> ${date} </p>`)
+})
 
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
+app.post('/api/persons', (request, response) => {
+  const body = request.body
     
-    if (body.name === undefined || body.number === undefined) {
-      return response.status(400).json({ 
-        error: 'name or number missing' })
-    }
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ 
+      error: 'name or number missing' })
+  }
     
-    const person = new Person({
-      name: body.name,
-      number: body.number
-    })
-
-    /*if (persons.find(person => person.name === body.name)) {
-      return response.status(409).json({
-          error: 'name already exists in the phonebook'
-      })
-      } */
-    
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number
   })
+    
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
+
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(people => {
@@ -59,18 +57,58 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
+    if (person) {
     response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+app.use(errorHandler)
   
 const PORT = process.env.PORT 
 app.listen(PORT, () => {
